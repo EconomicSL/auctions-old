1af778cd10d862e39a61e136be5c87b41056742a
davidrpugh <david.pugh@kapsarc.org>
2016-12-08 14:37:04 +0300

    Added a basic reverse auction type.

---
+++ src/main/scala/org/economicsl/auctions/ReverseAuction.scala
@@ -0,0 +1,41 @@
+/*
+Copyright 2016 EconomicSL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.economicsl.auctions
+
+import java.util.UUID
+
+import org.economicsl.auctions.orderbooks.OrderBook
+import org.economicsl.auctions.orders._
+
+
+sealed trait ReverseAuction[B <: LimitBidOrder with Quantity, A <: LimitAskOrder with Persistent with Quantity] {
+
+  type OB <: OrderBook[A, collection.GenIterable[(UUID, A)]]
+
+  def fill(order: B): Option[Fill]
+
+  def place(order: A): Unit
+
+}
+
+
+trait SingleUnitReverseAuction[B <: LimitBidOrder with SingleUnit, A <: LimitAskOrder with Persistent with SingleUnit]
+  extends ReverseAuction[B, A]
+
+
+trait MultiUnitReverseAuction[B <: LimitBidOrder with MultiUnit, A <: LimitAskOrder with Persistent with MultiUnit]
+  extends ReverseAuction[B, A]
+
