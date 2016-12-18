f6c92dd60dabf3167c145e6427d0d97576a743a2
davidrpugh <david.pugh@kapsarc.org>
2016-12-18 13:33:18 +0300

    removed type params

--- src/main/scala/org/economicsl/auctions/reverse/ReverseAuction.scala
+++ src/main/scala/org/economicsl/auctions/reverse/ReverseAuction.scala
@@ -27,7 +27,7 @@ sealed trait ReverseAuction[B <: LimitBidOrder with Quantity, A <: LimitAskOrder
 
   type OB <: OrderBook[A, collection.GenIterable[(UUID, A)]]
 
-  def fill(order: B): Option[Fill[A, B]]
+  def fill(order: B): Option[Fill]
 
   /** Place a `LimitAskOrder with Persistent with Quantity` into the `OrderBook`.
     *
