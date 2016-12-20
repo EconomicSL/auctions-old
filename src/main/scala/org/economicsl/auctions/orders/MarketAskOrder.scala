cdd19d30c214fe9df1dfa3a7387d93af21ea8bdd
davidrpugh <david.pugh@kapsarc.org>
2016-12-20 19:36:16 +0300

    Added market orders.

---
+++ src/main/scala/org/economicsl/auctions/orders/MarketAskOrder.scala
@@ -0,0 +1,54 @@
+/*
+Copyright 2016 EconomicSL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.economicsl.auctions.orders
+
+import java.util.UUID
+
+import org.economicsl.auctions.{Price, Quantity, Tradable}
+
+
+trait MarketAskOrder extends LimitAskOrder {
+  this: SinglePricePoint =>
+
+  val limit: Price = Price.MinValue
+
+}
+
+
+object MarketAskOrder {
+
+  def apply(issuer: UUID, quantity: Quantity, tradable: Tradable): MarketAskOrder with SinglePricePoint = {
+    SinglePricePointImpl(issuer, quantity, tradable)
+  }
+
+  def apply(issuer: UUID, tradable: Tradable): MarketAskOrder with SingleUnit = {
+    SingleUnitImpl(issuer, tradable)
+  }
+
+  private[this] case class SinglePricePointImpl(issuer: UUID, quantity: Quantity, tradable: Tradable)
+    extends MarketAskOrder with SinglePricePoint {
+
+    val isPersistent: Boolean = false
+
+  }
+
+  private[this] case class SingleUnitImpl(issuer: UUID, tradable: Tradable) extends MarketAskOrder with SingleUnit {
+
+    val isPersistent: Boolean = false
+
+  }
+
+}
