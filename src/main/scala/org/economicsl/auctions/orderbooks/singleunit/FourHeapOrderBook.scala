3fee9a8f0649e2f6bcdc32e87d53b9dbe981a243
davidrpugh <david.pugh@kapsarc.org>
2016-12-22 11:50:23 +0300

    Added license infor.

--- src/main/scala/org/economicsl/auctions/orderbooks/singleunit/FourHeapOrderBook.scala
+++ src/main/scala/org/economicsl/auctions/orderbooks/singleunit/FourHeapOrderBook.scala
@@ -1,3 +1,18 @@
+/*
+Copyright 2016 EconomicSL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.economicsl.auctions.orderbooks.singleunit
 
 import org.economicsl.auctions.orders.{LimitAskOrder, LimitBidOrder, SingleUnit}
@@ -30,7 +45,7 @@ case class FourHeapOrderBook private (matchedOrders: MatchedOrders, unMatchedOrd
       case (None, Some(bidOrder)) if order.limit <= bidOrder.limit =>
         new FourHeapOrderBook(matchedOrders + (order, bidOrder), unMatchedOrders - bidOrder)
       case (Some(askOrder), Some(_)) if order.limit < askOrder.limit =>
-        new FourHeapOrderBook(matchedOrders - askOrder + order, unMatchedOrders + askOrder)
+        new FourHeapOrderBook(matchedOrders.swap(askOrder, order), unMatchedOrders + askOrder)
       case _ =>
         new FourHeapOrderBook(matchedOrders, unMatchedOrders + order)
     }
@@ -43,7 +58,7 @@ case class FourHeapOrderBook private (matchedOrders: MatchedOrders, unMatchedOrd
       case (None, Some(askOrder)) if order.limit >= askOrder.limit =>
         new FourHeapOrderBook(matchedOrders + (askOrder, order), unMatchedOrders - askOrder)
       case (Some(bidOrder), Some(_)) if order.limit > bidOrder.limit =>
-        new FourHeapOrderBook(matchedOrders - bidOrder + order, unMatchedOrders + bidOrder)
+        new FourHeapOrderBook(matchedOrders.swap(bidOrder, order), unMatchedOrders + bidOrder)
       case _ =>
         new FourHeapOrderBook(matchedOrders, unMatchedOrders + order)
     }
--- src/main/scala/org/economicsl/auctions/orderbooks/singleunit/MatchedOrders.scala
+++ src/main/scala/org/economicsl/auctions/orderbooks/singleunit/MatchedOrders.scala
@@ -1,3 +1,18 @@
+/*
+Copyright 2016 EconomicSL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.economicsl.auctions.orderbooks.singleunit
 
 import org.economicsl.auctions.orders.{LimitAskOrder, LimitBidOrder, SingleUnit}
@@ -29,6 +44,14 @@ class MatchedOrders private (val askOrders: immutable.TreeSet[LimitAskOrder with
   def contains(order: LimitAskOrder with SingleUnit): Boolean = askOrders.contains(order)
 
   def contains(order: LimitBidOrder with SingleUnit): Boolean = bidOrders.contains(order)
+
+  def swap(existing: LimitAskOrder with SingleUnit, incoming: LimitAskOrder with SingleUnit): MatchedOrders = {
+    new MatchedOrders(askOrders - existing + incoming, bidOrders)
+  }
+
+  def swap(existing: LimitBidOrder with SingleUnit, incoming: LimitBidOrder with SingleUnit): MatchedOrders = {
+    new MatchedOrders(askOrders, bidOrders - existing + incoming)
+  }
 
 }
 
